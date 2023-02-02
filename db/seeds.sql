b9511e1005efcbbb1221aff67bbbe39048720769
BenjiCCB <bensdecker+CCB@gmail.com>
2023-02-02 16:08:49 -0500

    schema more

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,5 @@
-INSERT INTO students (first_name, last_name, enrolled)
-VALUES ("Elliot", "Smith", false),
-       ("Amira", "Afzal", true),
-       ("Christoper", "Lee", true),
-       ("Verónica", "Rodriguez", false),
-       ("Igor", "Stein", true);
+INSERT INTO departments (name)
+VALUES ("Sales"),
+       ("Ops"),
+       ("Products");
        
