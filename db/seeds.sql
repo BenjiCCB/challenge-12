931156492864cd33ba2b6bc7c9ed7f5a140a642b
BenjiCCB <bensdecker+CCB@gmail.com>
2023-02-02 17:32:24 -0500

    issue w seed

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,3 +7,9 @@ INSERT INTO roles (title, salary, department_id)
 VALUES ("Manager", 1000, 1),
        ("Engineer", 2000, 2),
        ("Marketer", 4000, 1);
+
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Bill", "Smith", 3, 2),
+       ("Jane", "Doe", 2, 1),
+       ("Mike", "Jones", 1, 1);
--- index.js
+++ index.js
@@ -3,43 +3,70 @@ const fs = require('fs');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
-function callManagerPrompts(){
+function callInitialPrompts(){
   inquirer
     .prompt([
       {
-        type: 'input',
-        name: 'name',
-        message: 'What is your manager\'s name?',
-      },    
-      {
-        type: 'input',
-        name: 'id',
-        message: 'What is your managers\'s ID?',
-      },
-      {
-        type: 'input',
-        name: 'email',
-        message: 'What is your manager\'s email?',
-      },
-      {
-        type: 'input',
-        name: 'office',
-        message: 'What is your managers\'s office number?',
-      },
-      {
         type: 'list',
-        name: 'more',
-        message: 'Would you like to add more employees?',
-        choices: ['Engineer', 'Intern', 'Complete team']
+        name: 'action',
+        message: 'What would you like to do?',
+        choices: ['View departments', 'View roles', 'View employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit app']
       }
     ])
 
   .then((data) => {
-   
-    console.log ("testing")
+
+    switch (data.action) {
+      case "View departments":
+        queryTable("departments");
+        break;
+      case "View roles":
+        queryTable("roles");
+        break;
+      case "View employees":
+        queryTable("employees");
+        break;
+      case "Add a department":
+        console.log("add dept");
+        break;
+      case "Add a role":
+        console.log("add role");
+        break;
+      case "Add an employee":
+        console.log("add emp")
+        break;
+      case "Update an employee role":
+        console.log("update emp")
+        break;
+      case "Update an employee role":
+        console.log("exit app")
+        break;
+      default:
+          console.log("error occured")
+    }
 
   });
 }
+
+
+// function callDepartmentPrompts(){
+//   inquirer
+//     .prompt([
+//       {
+//         type: 'list',
+//         name: 'action',
+//         message: "What is the name of the department you'd like to add?",
+//         choices: ['View departments', 'View roles', 'View employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit app']
+//       }
+//     ])
+
+//   .then((data) => {
+    
+
+
+//   });
+// }
+
 
 // Connect to database
 const db = mysql.createConnection(
@@ -49,15 +76,19 @@ const db = mysql.createConnection(
     user: 'root',
     // MySQL password
     password: 'ccb12345',
-    database: 'classlist_db'
+    database: 'org_db'
   },
   console.log(`Connected to the classlist_db database.`)
 );
 
 
 // Query database
-db.query('SELECT * FROM students', function (err, results) {
-  if(err){console.log(err)}
-  console.log('\n');
-  console.table(results);
-});
+function queryTable(table) {
+  db.query(`SELECT * FROM ${table}`, function (err, results) {
+    if(err){console.log(err)}
+    console.log('\n');
+    console.table(results);
+  });
+}
+
+callInitialPrompts()
